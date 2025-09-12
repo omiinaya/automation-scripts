c0c527d04ce01f8753a35672831e19ab00179efc
Code Server <omiinaya@gmail.com>
2025-09-12 14:47:24 +0000

    ask for admin when necessary

--- windows/toggle-location-services.ps1
+++ windows/toggle-location-services.ps1
@@ -1,6 +1,15 @@
-#Requires -RunAsAdministrator
-
 $ErrorActionPreference = "Stop"
+
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    try {
+        $arguments = "-NoProfile -ExecutionPolicy Bypass -Command `"& {cd '$pwd'; &'$($MyInvocation.MyCommand.Definition)'}`""
+        Start-Process PowerShell -Verb RunAs -ArgumentList $arguments
+        exit
+    } catch {
+        Write-Host "UAC prompt cancelled or failed"
+        exit 1
+    }
+}
 
 $locationService = Get-Service -Name "lfsvc" -ErrorAction SilentlyContinue
 if (-not $locationService) {
